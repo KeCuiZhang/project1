dfa017b3f6e77652cd932eeb5cd82bd2daf96c2b
KeCuiZhang <38581254+KeCuiZhang@users.noreply.github.com>
2018-12-28 12:19:49 +0800

    第一次提交

---
+++ pinyougou_sellergoods_interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,13 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * @Author kecuiZhang
+ * @Company http://www.springs.com
+ */
+public interface BrandService {
+    public List<TbBrand> findAll();
+}
